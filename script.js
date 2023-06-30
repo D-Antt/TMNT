a7f35eca2695e270438b104dd20c60b596d20e1a
Dan Anttila <115313515+D-Antt@users.noreply.github.com>
2023-06-30 14:09:23 -0400

    working on js some more

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var url = 'https://comicvine.gamespot.com/api/characters/?api_key=7d299aec6bf7c1968a01ca2db4367842401b5789&filter=universe:tmnt&format=json';
+var url = 'https://comicvine.gamespot.com/api/characters/?api_key=7d299aec6bf7c1968a01ca2db4367842401b5789&format=json';
+var characters;
 
 fetch(url)
 .then(response => {
@@ -9,21 +10,37 @@ fetch(url)
 })
 .then(data => {
     // Store all characters in an array
-    var characters = data.results;
+    characters = data.results;
 
-    // Function to get a random character
-    function getRandomCharacter() {
-        var randomIndex = Math.floor(Math.random() * characters.length);
-        return characters[randomIndex];
-    }
-
-    // Get two random characters
+    // Update the character images and names on your webpage with initial characters
     var character1 = getRandomCharacter();
     var character2 = getRandomCharacter();
-
-    // Update the character images and information on your webpage
-    // ...
+    document.getElementById('left-character-image').src = character1.image; // Replace 'image' with the actual property name for the image URL
+    document.getElementById('right-character-image').src = character2.image; // Replace 'image' with the actual property name for the image URL
+    document.getElementById('left-character-name').textContent = character1.name; // Replace 'name' with the actual property name for the name
+    document.getElementById('right-character-name').textContent = character2.name; // Replace 'name' with the actual property name for the name
 })
 .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
+});
+
+// Function to get a random character
+function getRandomCharacter() {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    return characters[randomIndex];
+}
+
+// Event listeners for the vote buttons
+document.getElementById('left-vote-button').addEventListener('click', function() {
+    var character = getRandomCharacter();
+    // Update the left character image and name on your webpage
+    document.getElementById('left-character-image').src = character.image; // Replace 'image' with the actual property name for the image URL
+    document.getElementById('left-character-name').textContent = character.name; // Replace 'name' with the actual property name for the name
+});
+
+document.getElementById('right-vote-button').addEventListener('click', function() {
+    var character = getRandomCharacter();
+    // Update the right character image and name on your webpage
+    document.getElementById('right-character-image').src = character.image; // Replace 'image' with the actual property name for the image URL
+    document.getElementById('right-character-name').textContent = character.name; // Replace 'name' with the actual property name for the name
 });
